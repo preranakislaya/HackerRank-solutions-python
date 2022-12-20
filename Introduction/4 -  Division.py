5a7a7e54a09db14bae76bc2013fd0bf06509da52
Prerana Kislaya <92728161+preranakislaya@users.noreply.github.com>
2022-12-21 00:28:05 +0530

    Add files via upload

---
+++ Introduction/4 -  Division.py
@@ -0,0 +1,17 @@
+# First approach
+
+if __name__ == '__main__':
+
+    a = int(input())
+    b = int(input())
+    print(int(a/b))
+    print(float(a/b))
+
+
+# Second approach
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
